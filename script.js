e59de64ed8bbf1faa16206a41cf639b77bb0ce07
alexmargita <alexmargita@gmail.com>
2025-09-26 16:14:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
 }
 
 const GITHUB_BASE_URL = "https://raw.githubusercontent.com/alexmargita/territory_cards-app/main/images/";
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyMLZV3i0Qm3xd8nbQZEJOTB-AA9LWhZw2HmKc9cZukjQLmq6T_85P14BkFveutwQN2/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxiwhtKhrgerDiXqozJqB7mY1GXQ9pNvxbvXatfq-9hnYDkJhT0K4YhUu6T__6xHtLR/exec";
 
 document.addEventListener('DOMContentLoaded', function() {
     const tg = window.Telegram.WebApp;
@@ -500,5 +500,4 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Початкове завантаження
     fetchAllData();
-
 });
