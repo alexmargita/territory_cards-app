9453a4d93bb87bb543aaab3bcd49d771bbd833e7
alexmargita <alexmargita@gmail.com>
2026-09-11 17:20:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'territory-cache-v11';
+const CACHE_NAME = 'territory-cache-v12';
 const GITHUB_IMAGES_URL_PATTERN = /^https:\/\/raw\.githubusercontent\.com\/alexmargita\/territory_cards-app\/main\/images\//;
 
 const PRECACHE_URLS = [
